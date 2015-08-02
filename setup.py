249921b8533e8d5481ec1bcdf2372f291f759f8e
Alex Khomchenko <akhomchenko@gmail.com>
2015-08-02 14:07:25 +0300

    py3 support is fixed, test infrastructure is added
    * fix py3 support
    * add tox
    * add editorconfig
    * add venv and ide ignore

--- setup.py
+++ setup.py
@@ -10,15 +10,22 @@ setup(name='moves',
       url='https://github.com/lysol/moves',
       download_url='http://github.com/lysol/moves/archive/v0.1.tar.gz',
       packages=['moves'],
-      install_requires=open('requirements.txt').read(),
+      install_requires=[
+          'requests>=0.11'
+      ],
       long_description=open('README.md').read(),
+      include_package_data=True,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       license='MIT'
---
+++ tests/test_moves.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+
+
+import moves  # TODO: write tests, this one checks compatibility
