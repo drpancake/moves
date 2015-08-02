39138fd22981cf296b68fdf67d7314d756ec9861
Alex Khomchenko <akhomchenko@gmail.com>
2015-08-02 14:58:54 +0300

    add sample test

--- tests/test_moves.py
+++ tests/test_moves.py
@@ -1,4 +1,15 @@
 # coding: utf-8
 
 
-import moves  # TODO: write tests, this one checks compatibility
+import pytest
+
+import moves
+
+
+class TestMovesClient(object):
+    def test_raises_if_no_acess_token_is_provided(self):
+        client = moves.MovesClient()
+        with pytest.raises(moves.MovesAPIError) as err:
+            client.api('/user')
+
+        assert 'provide a valid access token' in str(err.value)
